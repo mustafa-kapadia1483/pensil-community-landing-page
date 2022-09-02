353f5f579a631d2b91444ee341f8f8ca9de71356
Mustafa Kapadia <mustafakapadia76@gmail.com>
2022-09-02 14:11:29 +0530

    padding issue fix

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -117,8 +117,7 @@ export default function Layout({ children }) {
           paddingInline: opened && 0,
         },
       }}
-      navbarOffsetBreakpoint="xl"
-      asideOffsetBreakpoint="xl"
+      // navbarOffsetBreakpoint="xl"
       navbar={
         <Navbar
           p="md"
